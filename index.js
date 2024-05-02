26f8b33156db86b995b6bec2586f15d58cacca87
weeb.js <weeb.js@yahoo.com>
2024-05-02 18:57:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require("@discordjs/voice");
+const express = require('express');
+const port = process.env.PORT || 4000;
 
 const client = new Client({ checkUpdate: false });
 
@@ -7,6 +9,16 @@ require('dotenv').config();
 const token = process.env.TOKEN;
 const guildId = process.env.GUILD_ID;
 const channelId = process.env.CHANNEL_ID;
+
+const app = express();
+app.get("/", (req, res) => {
+    res.status(200).send({
+        success: "true"
+    });
+});
+app.listen(port);
+
+
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
